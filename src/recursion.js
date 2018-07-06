f073279a846670edd97c1ecb2bcdaeaa18bf059c
Maria Octavo <maria.octavo13@gmail.com>
2018-07-06 11:42:58 -0500

    Second Commit

--- src/recursion.js
+++ src/recursion.js
@@ -7,39 +7,70 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  // if the given number equals zero
   if (n === 0) {
-  return 1;
+    /* return 1  because you cant return 0 because 0 * anything will always be 0*/
+    return 1;
+  // If the number is less than 0, return null
 } else if (n < 0) {
   return null;
 }
+  // return the product factorial
   return (n * factorial(n - 1));
 };
-
+/*******************************************************************/
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return array [0] + sum(array.slice(1));
 };
-
+/*******************************************************************/
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var sum = 0;
+  for (var i = 0; i < array.length; i++) {
+     if (array[i] instanceof Array) { sum += arraySum(array[i]);}
+     if (array[i] === Math.round(array[i])) {sum += array[i];}
+  }
+ return sum;
 };
-
+/*******************************************************************/
 // 4. Check if a number is even.
 var isEven = function(n) {
-};
+  if(n === 0){
+    return true
+  }else if(n  === 1){
+    return false
+  }
 
+  return isEven (Math.abs(n) - 2);
+};
+/*******************************************************************/
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  if (n === 0) { // base case
+    return 0;
+  }
+  n = n > 0 ? n - 1 : n + 1; // recursive case
+  return n + sumBelow(n);
 };
-
+/*******************************************************************/
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
-};
+  if (y - x === 1 || y - x === 0) {  // base case
+    return [];
+  }
+   return x * range(x + 1, y)
 
+};
+/*******************************************************************/
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
